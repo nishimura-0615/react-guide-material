ac1ea6f3de0b1e3490589013bf00125efcb8bfc8
西村健太 <>
2024-04-20 23:51:05 +0900

    2024_04_20　4月20日 課題勉強分

--- 05_state_and_event/src/060_state_object/start/Example.jsx
+++ 05_state_and_event/src/060_state_object/start/Example.jsx
@@ -3,9 +3,14 @@ import { useState } from "react";
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
   const [person, setPerson] = useState(personObj);
-
+  //オブジェクトの値を変更する場合は新しいオブジェクトを生成し、その中のプロパティを設定する
+  //そのために({})を使って配列（オブジェクト)として定義している
   const changeName = (e) => {
-    setPerson({ name: e.target.value, age:person.age })
+    person.name = '';
+    console.log({ ...person, name:e.target.value });
+    //setPerson({ name: person.name, age:e.target.value })
+    //setPerson　スプレッド演算子で書き換える際はどのオブジェクトでどのプロパティか明示する。
+    setPerson({ ...person, name:e.target.value })
   }
 
   const changeAge = (e) =>{
