2b9fe037234020b826b7e50b3eaf05903a9d8547
西村健太 <>
2024-04-21 23:46:06 +0900

    2024_04_21 4月21日個人学習

--- 06_control_and_form/src/010_list_components/start/Example.jsx
+++ 06_control_and_form/src/010_list_components/start/Example.jsx
@@ -2,12 +2,31 @@
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  // 配列の表示方法
 
+  // const animalList = [];
+  // for(const animal of animals) {
+  //   animalList.push(<li>{animal}</li>)
+  // }
+
+  //mapを使った表示方法　mapの場合は式になるのでJSX内で記載できる
+  const helloAnimals = animals.map((animal) =><li>Hello, {animal}</li>)
   return (
     <>
       <h3>配列の操作</h3>
+
       <ul>
-        
+        {/* for文をulタグ内に記載するとエラーしてしまう。
+        あくまでJSXないは式を書くと画面出力されるため、文を書くとエラーしてしまう */}
+        {/* for(const animal of animals) {
+        animalList.push(<li>{animal}</li>)
+        } */}
+        {/* <li>{animals[0]}</li>
+        <li>{animals[1]}</li>
+        <li>{animals[2]}</li> */}
+        {/* {animalList} */}
+        {/* { helloAnimals } */}
+        {animals.map((animal) => <li>Hello, {animal}</li>)}
       </ul>
     </>
   );
--- 06_control_and_form/src/040_list_and_filter/start/Example.jsx
+++ 06_control_and_form/src/040_list_and_filter/start/Example.jsx
@@ -3,13 +3,17 @@ import { useState } from "react";
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  const [ filterVal,setFilterVal ] = useState("");
   return (
     <>
       <h3>配列のフィルター</h3>
+      <input type="text" value={filterVal} onChange={(e) => setFilterVal(e.target.value)}/>
       <ul>
-        {animals
+        {/*.filterメソッドの処理は-1の場合は見つかりませんでした。と出力し、
+        -1以外の場合は、条件式としてTRUEを返すようにしている  */}
+        {animals.filter(animal => animal.indexOf(filterVal) !== -1)
           .map((animal) => (
-          <li>{animal}</li>
+          <li key={animal}>{animal}</li>
         ))}
       </ul>
     </>
