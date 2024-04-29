6e399a84a6ffb6bbab25cb9583edc48f17ea8c5b
西村健太 <>
2024-04-29 23:31:14 +0900

    2024_4_29 4月29日 自己学習分

--- 21_nextjs_app_router/end/src/app/020_SG/[id]/page.js
+++ 21_nextjs_app_router/end/src/app/020_SG/[id]/page.js
@@ -1,8 +1,12 @@
-import { paths } from "../paths";
+import { paths } from '../paths';
 
 export default function Page({ params }) {
-  const date = new Date;
-  return <h3>このページは{params.id}です。{date.toJSON()}</h3>;
+  const date = new Date();
+  return (
+    <h3>
+      このページは{params.id}です。{date.toJSON()}
+    </h3>
+  );
 }
 
 export async function generateStaticParams() {
--- 21_nextjs_app_router/start/src/app/010_SSR/page.js
+++ 21_nextjs_app_router/start/src/app/010_SSR/page.js
@@ -1,9 +1,11 @@
-import './lib';
 import ClientComp from './components/ClientComp';
 import { ENDPOINT } from '@/constants';
 import ArticleList from '@/components/articleList';
 
+// http://locakhost:4020/010/SSR/2
+// http://locakhost:4020/010/SSR/5
 export default async function SSR() {
+  // 基本next.jsではキャッシュが貯まる仕様となっている
   const articles = await fetch(ENDPOINT, {
     next: { revalidate: 10 },
   }).then((res) => res.json());
